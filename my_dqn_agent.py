db8abff1f2a47759c6511a8137d655d081ae6757
nudibranch <armorlaruan@gmail.com>
2021-03-08 19:44:14 +0800

    trying to do torch dqn

--- my_dqn_agent.py
+++ my_dqn_agent.py
@@ -13,6 +13,9 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class DQN:
     def __init__(self,env):
         self.env = env
@@ -25,9 +28,37 @@ class DQN:
         self.tau = 0.05
         self.model = self.create_model()
         self.target_model =  self.create_model()
+        self.target_model.load_state_dict(self.model.state_dict())
     def create_model(self):
         model = nn.Sequential(
             nn.Linear(self.env.observation_space.shape[0],64),
             nn.ReLU(),
             nn.Linear(64, self.env.action_space.n),
         )
+        return model.to(device)
+    def action(self,state):
+        self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        act = torch.argmax(self.model(state)[0])
+        return act
+    def remember(self,state):
+        self.memory.append([state,action,reward,new_state,done])
+    def replay(self, transitions):
+        batch_size = 32
+        if len(self.memory) < batch_size:
+            return
+        
+        
+
+    def train_target_model(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+    def train_model(self, conc_states, targets):
+        y_preds = self.model(conc_states)
+        loss = nn.MSEloss()
+        self.optimizer = optim.RMSprop(self.model.parameters())
+
+        optimizer.zero_grad()
+        loss.backward()
+
+    
